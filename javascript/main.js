ecdea9047727c405c16fd2c6e18626a97a6b171b
LoganKollerLakeside <95874377+LoganKollerLakeside@users.noreply.github.com>
2021-12-09 10:47:53 -0600

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,26 @@
-function getTimeUntilChristmas()
+function start_worker()
 {
-    const minute = 1000 * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    const year = day * 365;
-    const christmas2021 = new Date(2021, 11, 25);
-    const currentDate = new Date();
-    return Math.round((christmas2021 - currentDate) / day)
+    function getTimeUntilChristmas()
+    {
+        const minute = 1000 * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+        const year = day * 365;
+        const christmas2021 = new Date(2021, 11, 25);
+        const currentDate = new Date();
+        return Math.round((christmas2021 - currentDate) / day)
+    }
+
+    var lastTick = new Date().getTime();
+    while (true) {
+        if ((new Date().getTime() - lastTick) >= 1000)
+        {
+            lastTick = new Date().getTime()
+            document.getElementById("countdowntext").innerHTML = "Time until Christmas: " + getTimeUntilChristmas()      
+        }
+    }
+
 }
 
-var lastTick = new Date().getTime();
-while (true) {
-    if ((new Date().getTime() - lastTick) >= 1000)
-    {
-        lastTick = new Date().getTime()
-        document.getElementById("countdowntext").innerHTML = "Time until Christmas: " + getTimeUntilChristmas()      
-    }
-}
+if (window != self)
+    start_worker();
